276590f3985473b453d3f103cfbab5adb46b737c
confirmation-jenssy <confirmation@outsourceengage.com>
2026-06-18 09:24:08 -0700

    Add configuration for board and statuses

---
+++ config.py
@@ -0,0 +1,27 @@
+BOARD_ID = 1962914669
+
+COLUMN_IDS = {
+    "meeting_date": "date_mkr2q53p",
+    "status": "status",
+    "phone": "text_mkr27gh0",
+    "address": "text_mkr2an4n",
+    "project": "long_text_mkr2wjqk",
+    "source": "text_mkr22s20",
+    "same_day": "color_mkr2rpkj"
+}
+
+CONFIRMED_STATUSES = {
+    "TOMMY",
+    "ELITE",
+    "UNIVERSAL"
+}
+
+TOTAL_LEAD_STATUSES = {
+    "TOMMY",
+    "ELITE",
+    "UNIVERSAL",
+    "NO ANSWER",
+    "CANCELED",
+    "CANCELLED",
+    "RESCHEDULE"
+}
