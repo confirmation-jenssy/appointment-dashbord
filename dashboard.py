6c877686e5bea915fa453003329491a77a34e25d
confirmation-jenssy <confirmation@outsourceengage.com>
2026-06-19 20:14:50 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -132,25 +132,25 @@ if page == "Appointment Counts":
 
     st.write("Today =", today)
 
-today_count = 0
-
-for item in items:
-
-    values = {}
-
-    for col in item["column_values"]:
-        values[col["id"]] = col["text"]
-
-    meeting_date = values.get(
-        COLUMN_IDS["meeting_date"],
-        ""
-    )
-
-    dt = parse_meeting_date(meeting_date)
-
-    if dt and dt.date() == today:
-        today_count += 1
-
-st.write("Appointments for today =", today_count)
+    today_count = 0
+    
+    for item in items:
+    
+        values = {}
+    
+        for col in item["column_values"]:
+            values[col["id"]] = col["text"]
+    
+        meeting_date = values.get(
+            COLUMN_IDS["meeting_date"],
+            ""
         )
-    )
+    
+        dt = parse_meeting_date(meeting_date)
+    
+        if dt and dt.date() == today:
+            today_count += 1
+    
+    st.write("Appointments for today =", today_count)
+            )
+        )
