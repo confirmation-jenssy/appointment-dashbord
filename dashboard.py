39ea72790ec5bc346c7e497eaff5cfa460faec0a
confirmation-jenssy <confirmation@outsourceengage.com>
2026-06-19 19:12:46 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -124,6 +124,19 @@ if page == "Appointment Counts":
 
     st.title("Appointment Counts")
 
+    st.write("Server Today:", datetime.now().date())
+
+    for item in items[:10]:
+
+    values = {}
+
+    for col in item["column_values"]:
+        values[col["id"]] = col["text"]
+
+    st.write(
+        values.get("date_mkr2q53p", "")
+    )
+    
     counts = build_appointment_counts(items)
 
     st.write(counts)
