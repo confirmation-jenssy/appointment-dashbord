bebc3c6641e731f7e7e8df5d92b900ab9028fa54
confirmation-jenssy <confirmation@outsourceengage.com>
2026-06-21 01:12:46 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -482,15 +482,36 @@ if page == "Appointment Counts":
         st.markdown(
             f"**CA {get_status(tomorrow_ca, socal_target)}**"
         )
-        
-        st.write(
-            f"10-12: {counts['socal']['tomorrow']['10-12']}"
+
+        slot_target = round(
+            socal_target / 3
         )
-        
+
+        slot_booked = counts["socal"]["tomorrow"]["10-12"]
+
         st.write(
-            f"1-3: {counts['socal']['tomorrow']['1-3']}"
+            f"10AM-12PM: {slot_booked}/{slot_target} "
+            f"{get_slot_status(slot_booked, slot_target)}"
         )
+
+        slot_target = round(
+            socal_target / 3
+        )
+
+        slot_booked = counts["socal"]["tomorrow"]["1-3"]
+
+        st.write(
+            f"1PM-3PM: {slot_booked}/{slot_target} "
+            f"{get_slot_status(slot_booked, slot_target)}"
+        )
+
+        slot_target = round(
+            socal_target / 3
+        )
+
+        slot_booked = counts["socal"]["tomorrow"]["4-6"]
         
         st.write(
-            f"4-6: {counts['socal']['tomorrow']['4-6']}"
+            f"4PM-6PM: {slot_booked}/{slot_target} "
+            f"{get_slot_status(slot_booked, slot_target)}"
         )
