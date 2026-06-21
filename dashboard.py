8eef3699b45f15b9827858b7430b8b5aa2a9e82f
confirmation-jenssy <confirmation@outsourceengage.com>
2026-06-21 00:48:36 -0700

    Refactor dashboard.py to enhance readability
    Removed unnecessary blank lines and improved code readability.

--- dashboard.py
+++ dashboard.py
@@ -325,9 +325,7 @@ if page == "Appointment Counts":
         slot_target = round(
             oregon_target / 3
         )
-        
         slot_booked = counts["oregon"]["today"]["10-12"]
-        
         st.write(
             f"10AM-12PM: {slot_booked}/{slot_target} "
             f"{get_slot_status(slot_booked, slot_target)}"
@@ -336,9 +334,7 @@ if page == "Appointment Counts":
         slot_target = round(
             oregon_target / 3
         )
-        
         slot_booked = counts["oregon"]["today"]["1-3"]
-        
         st.write(
             f"1PM-3PM: {slot_booked}/{slot_target} "
             f"{get_slot_status(slot_booked, slot_target)}"
@@ -347,9 +343,7 @@ if page == "Appointment Counts":
         slot_target = round(
             oregon_target / 3
         )
-        
         slot_booked = counts["oregon"]["today"]["4-6"]
-        
         st.write(
             f"4PM-6PM: {slot_booked}/{slot_target} "
             f"{get_slot_status(slot_booked, slot_target)}"
